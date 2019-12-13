237373bb65a20a394e2813743f1ff2e58b999db0
dani-ddl <dani--ef-@hotmail.com>
2019-12-13 09:39:25 +0100

    oculto el campo en ventas y compras

--- __manifest__.py
+++ __manifest__.py
@@ -25,8 +25,7 @@
     'name': 'Res Partner Show Ref',
     'summary': """show customer reference""",
     'version': '12.0.1.0.0',
-    'description': """
-allows to display the field in the client file as well as edit it""",
+    'description': """allows to display the field in the client file as well as edit it""",
     'author': 'DDL',
     'company': 'Xtendoo',
     'website': 'http://www.xtendoo.com',
